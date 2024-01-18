a13706f240e58f4719bacfb9b3364ad1f5530cae
Octol1ttle <l1ttleofficial@outlook.com>
2024-01-18 19:45:56 +0500

    fucking forge

--- forge/src/main/java/ru/octol1ttle/knockdowns/forge/KnockdownsForge.java
+++ forge/src/main/java/ru/octol1ttle/knockdowns/forge/KnockdownsForge.java
@@ -1,7 +1,7 @@
 package ru.octol1ttle.knockdowns.forge;
 
 import dev.architectury.platform.forge.EventBuses;
-import net.minecraftforge.eventbus.api.SubscribeEvent;
+import net.minecraftforge.eventbus.api.IEventBus;
 import net.minecraftforge.fml.common.Mod;
 import net.minecraftforge.fml.event.lifecycle.FMLClientSetupEvent;
 import net.minecraftforge.fml.javafmlmod.FMLJavaModLoadingContext;
@@ -13,11 +13,13 @@ import ru.octol1ttle.knockdowns.common.KnockdownsCommon;
 public class KnockdownsForge {
     public KnockdownsForge() {
         // Submit our event bus to let architectury register our content on the right time
-        EventBuses.registerModEventBus(KnockdownsCommon.MOD_ID, FMLJavaModLoadingContext.get().getModEventBus());
+        IEventBus modEventBus = FMLJavaModLoadingContext.get().getModEventBus();
+        EventBuses.registerModEventBus(KnockdownsCommon.MOD_ID, modEventBus);
+        modEventBus.addListener(this::onInitializeClient);
+
         KnockdownsCommon.init();
     }
 
-    @SubscribeEvent
     public void onInitializeClient(FMLClientSetupEvent event) {
         KnockdownsClient.init();
     }
